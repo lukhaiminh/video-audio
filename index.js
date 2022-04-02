caa10c3fee4a701b15c3545e1937d794489d3ec7
kimjongun83 <quocdattranhuu1606@gmail.com>
2022-04-02 23:25:46 +0700

    fix bug cut for require

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const ffmpeg = require('fluent-ffmpeg');
 const videoFile = './expressjs.mp4';
 ffmpeg.ffprobe(videoFile,(err,metaData)=>{
-    const { duration } = metaData.format;
+    const {duration} = metaData.format;
     console.log(duration);
-    const startingTime = parseInt(duration /2 );
-    const clipDuration = 10;
-
+    // tính ở thời điểm giây thứ 25
+    const startingTime = parseInt(duration*25/120);
+    const clipDuration = 19;
     ffmpeg()
         .input(videoFile)
-      
-        
-        .outputOptions([`-ss 4`,`-t 7`])
-    
-        .output('./end01_result.mp4')
+        .outputOptions([`-ss ${startingTime}`,`-t ${clipDuration}`])
+         .output('./end7_result.mp4')
         .on('end',()=>console.log('Done'))
         .on('error',(err)=>console.log(err))
         .run();
