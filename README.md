d45aeb73669d67f44adaeacee1291c59633a1fe2
MooN <moon.lu@zillearn.com>
2022-04-04 20:59:28 +0700

    Update trim video function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # processvideo
+
+How to Install FFmpeg on Windows: https://www.wikihow.com/Install-FFmpeg-on-Windows
--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const ffmpeg = require('fluent-ffmpeg');
-const videoFile = './expressjs.mp4';
-ffmpeg.ffprobe(videoFile,(err,metaData)=>{
-    const {duration} = metaData.format;
-    console.log(duration);
-    // tính ở thời điểm giây thứ 25
-    const startingTime = parseInt(duration*25/120);
-    const clipDuration = 19;
+
+// const INPUT = './public/input/video-less-100MB.mp4';
+const OUTPUT_PATH = './public/output/out.mp4';
+
+function trimVideo(filePath, startingTime, duration) {
+  // TODO: Dat convert below func to Promise or async / await
+  ffmpeg.ffprobe(filePath, (err, _metaData) => {
+    if (err) {
+      throw new Error('file is not esxits');
+    }
+
     ffmpeg()
-        .input(videoFile)
-        .outputOptions([`-ss ${startingTime}`,`-t ${clipDuration}`])
-         .output('./end7_result.mp4')
-        .on('end',()=>console.log('Done'))
-        .on('error',(err)=>console.log(err))
-        .run();
-})
+      .input(filePath)
+      .outputOptions([`-ss ${startingTime}`, `-t ${duration}`])
+      .output(OUTPUT_PATH)
+      .on('end', () => {
+        console.log('Done');
+        // add sticker
+        // ffmpeg.ffprobe(filePath, (err, _metaData) => {
+        //   if (err) {
+        //     throw new Error('sticker err');
+        //   }
+        // }).on('end', () => {
+        //   ffmpeg.ffprobe(filePath, (err, _metaData) => {
+
+        //   });
+        // });
+      })
+      .on('error', (err) => console.log(err))
+      .run();
+  });
+}
+
+// function addSticker()
+
+trimVideo('./public/input/video-less-100MB.mp4', 10, 15);
