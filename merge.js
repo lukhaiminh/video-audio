f880419fc32aede5ccb19531bc2b6562678f3405
havefun <20127547@student.hcmus.edu.vn>
2022-04-06 17:41:38 +0700

    update merge-video

--- merge.js
+++ merge.js
@@ -1,31 +1,23 @@
 const ffmpeg = require('fluent-ffmpeg');
-const fs = require('fs'); 
-const { exit } = require('process');
 filePath1='video/video1.mp4'
 filePath2='video/video2.mp4'
 fileOut=`video/${Date.now()}.mp4`
-const readFilePromise = (filePath) => {
-  return new Promise ((resolve, reject) => {
-    fs.readFile(filePath, (err, data) => {
-      if (err) return reject(err)
-      resolve(data)
+
+const tmp = () => {
+  return new Promise((resolve, reject) => {
+    ffmpeg().input(filePath1)
+    .input(filePath2)
+    .mergeToFile(fileOut)
+    .on('end', function() {
+      resolve('Done')
+    })
+    .on('error', function(err) {
+      reject(err)
     })
   })
 }
 
- function pro(){
-      ffmpeg(filePath1).input(filePath2).mergeToFile(fileOut)
-    }
-async function merge()
-{
-  let loadFile1=readFilePromise(filePath1).then(data=>console.log('Load success f1')).catch(err=>console.log(err))
-  let loadFile2=readFilePromise(filePath2).then(data=>console.log('Load success f2')).catch(err=>console.log(err))
-  let tmp=await Promise.all([loadFile1,loadFile2])
-  //check isvalid
-  let res=await pro()
-  return res;
-}
-
-merge();
+tmp().then(res=>console.log(res)).catch(err=>console.log(err))
+  
 
 
