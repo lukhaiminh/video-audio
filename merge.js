c299f588ca81148119a2f5ff335c39c224c64f15
havefun <20127547@student.hcmus.edu.vn>
2022-04-07 19:32:48 +0700

    Update new merge video

--- merge.js
+++ merge.js
@@ -1,25 +1,34 @@
 const ffmpeg = require('fluent-ffmpeg');
-filePath1='video/video1.mp4'
-filePath2='video/video2.mp4'
-fileOut=`video/${Date.now()}.mp4`
+filePath1 = './public/input/video1.mp4';
+filePath2 = './public/input/video2.mp4';
+fileOut = `./public/output/`;
 
-const tmp = () => {
+const mergePromise = (filePath1, filePath2, fileOut) => {
   return new Promise((resolve, reject) => {
-    ffmpeg().input(filePath1)
-    .input(filePath2)
-    .mergeToFile(fileOut)
-    .on('end', function() {
-      resolve('Done')
-    })
-    .on('error', function(err) {
-      reject(err)
-    })
-  })
+    const extensions = filePath1.split('.');
+    const ext = extensions[extensions.length - 1];
+
+    const extensions1 = filePath2.split('.');
+    const ext1 = extensions1[extensions1.length - 1];
+
+    if (ext === ext1) {
+      console.log('The extension is valid');
+    } else {
+      throw new Error('Input is not valid'); //the temporary solution for validity file input
+    }
+
+    ffmpeg()
+      .input(filePath1)
+      .input(filePath2)
+      .mergeToFile(`${fileOut}${Date.now()}.${ext}`)
+      .on('end', resolve(`${fileOut}${Date.now()}.${ext}`))
+      .on('error', reject(err));
+  });
+};
+
+function merge(filePath1, filePath2, fileOut) {
+  mergePromise(filePath1, filePath2, fileOut)
+    .then((file) => console.log(file))
+    .catch((err) => console.log(err));
 }
-function merge(){
-tmp().then(res=>console.log(res)).catch(err=>console.log(err))
-}
-merge()
-
-
-
+merge(filePath1, filePath2, fileOut);
