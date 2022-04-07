35d3c2c96d9f3845c87f9d18235b3afe823f1c8a
havefun <20127547@student.hcmus.edu.vn>
2022-04-08 01:43:57 +0700

    Replace merge video

--- actions/merge.js
+++ actions/merge.js
@@ -1,34 +1,38 @@
+const errors = require('bluebird/js/release/errors');
 const ffmpeg = require('fluent-ffmpeg');
 filePath1 = './public/input/video1.mp4';
 filePath2 = './public/input/video2.mp4';
-fileOut = `./public/output/`;
-
-const mergePromise = (filePath1, filePath2, fileOut) => {
+//you can input file nth
+const fileOut = `./public/output/`;
+const mergePromise = (...arguements) => {
   return new Promise((resolve, reject) => {
-    const extensions = filePath1.split('.');
-    const ext = extensions[extensions.length - 1];
-
-    const extensions1 = filePath2.split('.');
-    const ext1 = extensions1[extensions1.length - 1];
-
-    if (ext === ext1) {
-      console.log('The extension is valid');
-    } else {
-      throw new Error('Input is not valid'); //the temporary solution for validity file input
+    if (arguements.length === 0) {
+      throw new Error('There are no data input');
     }
-
-    ffmpeg()
-      .input(filePath1)
-      .input(filePath2)
-      .mergeToFile(`${fileOut}${Date.now()}.${ext}`)
+    const extensions = arguements[0].split('.');
+    const ext = extensions[extensions.length - 1];
+    let parseFile = [];
+    let extension = '';
+    for (i = 1; i < arguements.length; i++) {
+      parseFile = arguements[i].split('.');
+      extension = parseFile[parseFile.length - 1];
+      if (extension != ext) {
+        throw new Error('Input is not valid');
+      }
+    }
+    const FFmpeg = ffmpeg();
+    for (i in arguements) {
+      FFmpeg.input(arguements[i]);
+    }
+    FFmpeg.mergeToFile(`${fileOut}${Date.now()}.${ext}`)
       .on('end', resolve(`${fileOut}${Date.now()}.${ext}`))
       .on('error', reject(err));
   });
 };
 
-function merge(filePath1, filePath2, fileOut) {
-  mergePromise(filePath1, filePath2, fileOut)
+function merge(...arguements) {
+  mergePromise(...arguements)
     .then((file) => console.log(file))
     .catch((err) => console.log(err));
 }
-merge(filePath1, filePath2, fileOut);
+merge(filePath1, filePath2);
