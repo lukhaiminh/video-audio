ab0c4fa4376ef5f7645cb2b925bb61c560d0a351
kimjongun83 <quocdattranhuu1606@gmail.com>
2022-04-10 01:44:30 +0700

    Convert to promise from callback

--- actions/subtitles.js
+++ actions/subtitles.js
@@ -1,2 +1,21 @@
 const ffmpeg = require("fluent-ffmpeg");
-filePath ='./'
+const OUTPUT_FOLDER_PATH = './public/output/';
+
+function addSubtitle(filePath){
+   const extensions = filePath.split('.');
+   const ext = extensions[extensions.length - 1];
+    return new Promise((resolve,reject)=>{
+       ffmpeg(filePath)
+       .outputOptions('-vf subtitles=./subtitle.srt')
+       .save(`${OUTPUT_FOLDER_PATH}${Date.now()}.${ext}`)
+       .on('end', ()=>{
+          console.log('Done Add Subtitles')
+          return resolve()
+        })
+        .on('err',(err)=>{
+            return reject(err)
+        })
+    })
+}
+const INPUT = './public/input/demosub.mp4';
+addSubtitle(INPUT);
